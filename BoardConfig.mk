89c8f0bea46a1c673fa9bf1ae8bb536286842911
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-26 02:24:16 +0800

    Mi8953: Refactor partition flags
    * Set file system type and copy out for each partition
    * Set reserved size with a foreach loop
    
    Co-authored-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,13 @@
 #
 
 # Partitions
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+SSI_PARTITIONS := system
+TREBLE_PARTITIONS := vendor
+ALL_PARTITIONS := $(SSI_PARTITIONS) $(TREBLE_PARTITIONS)
+
+$(foreach p, $(call to-upper, $(ALL_PARTITIONS)), \
+    $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := ext4) \
+    $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
 
 # Inherit from common mithorium-common
 include device/xiaomi/mithorium-common/BoardConfigCommon.mk
@@ -40,6 +46,7 @@ BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 55087422464
 
+# Partitions - dynamic
 BOARD_SUPER_PARTITION_BLOCK_DEVICES := cust system
 BOARD_SUPER_PARTITION_METADATA_DEVICE := system
 BOARD_SUPER_PARTITION_CUST_DEVICE_SIZE := 536870912
@@ -48,7 +55,18 @@ BOARD_SUPER_PARTITION_SIZE := $(shell expr $(BOARD_SUPER_PARTITION_CUST_DEVICE_S
 
 BOARD_SUPER_PARTITION_GROUPS := mi8953_dynpart
 BOARD_MI8953_DYNPART_SIZE := $(shell expr $(BOARD_SUPER_PARTITION_SIZE) - 4194304 )
-BOARD_MI8953_DYNPART_PARTITION_LIST := system vendor
+BOARD_MI8953_DYNPART_PARTITION_LIST := $(ALL_PARTITIONS)
+
+# Partitions - reserved size
+$(foreach p, $(call to-upper, $(SSI_PARTITIONS)), \
+    $(eval BOARD_$(p)IMAGE_EXTFS_INODE_COUNT := 6144))
+$(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
+    $(eval BOARD_$(p)IMAGE_EXTFS_INODE_COUNT := 4096))
+
+$(foreach p, $(call to-upper, $(SSI_PARTITIONS)), \
+    $(eval BOARD_$(p)IMAGE_PARTITION_RESERVED_SIZE := 209715200)) # 200 MB
+$(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
+    $(eval BOARD_$(p)IMAGE_PARTITION_RESERVED_SIZE := 41943040)) # 40 MB
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
