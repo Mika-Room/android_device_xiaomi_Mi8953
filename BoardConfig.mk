48f59cc372168dec6d8276a0dea855ffddb69932
Yumi Yukimura <me.cafebabe@gmail.com>
2025-05-10 17:57:08 +0800

    Mi8953: Drop `TARGET_RECOVERY_DEVICE_MODULES`
    * It's deprecated on 15 QPR2
    * Building a library as module doesn't get it executed? and libinit
      still does its job in recovery mode without migrating this
    
    Change-Id: I7be000356fe7d7bd099615d334892d1686bece0e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,6 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_mi8953
-TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_mi8953
 
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.android_dt_dir=/non-existent androidboot.boot_devices=soc/7824900.sdhci
